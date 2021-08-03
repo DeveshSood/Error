40a7b852abeb59aaa74207e6dac07b859c6e3eda
DeveshSood <77494434+DeveshSood@users.noreply.github.com>
2021-08-03 21:38:14 +0530

    Add files via upload

---
+++ screens/Home.js
@@ -0,0 +1,96 @@
+import React, { Component } from 'react';
+import { Text, View, StyleSheet, SafeAreaView, TouchableOpacity, StatusBar, ImageBackground,Image} from 'react-native';
+
+export default class HomeScreen extends Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <SafeAreaView styles={styles.droidSafeArea}/>
+                <ImageBackground style={styles.backgroundImage} source={require('../assets/bg.png')}>
+                <View style={styles.titleBar}>
+                    <Text style={styles.titleText}>ISS Tracker App</Text>
+                    
+                </View>
+                <TouchableOpacity style={styles.routeCard}
+                    onPress={()=>
+                        this.props.navigatio.navigate("ISSLocation")
+                    }>
+                    <Text style={styles.routeText}>ISS Locator</Text>
+                    <Text style={styles.knowMore}>{"Know More-->"}</Text>
+                    <Text style={styles.bgDigit}>1</Text>
+                    <Image source = {reqire('../assets/iss_icon.png')} style={styles.iconImage}></Image>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.routeCard}
+                    onPress={()=>
+                        this.props.navigatio.navigate("Meteors")
+                    }>
+                    <Text style={styles.routeText}>Meteors</Text>
+                    <Text style={styles.knowMore}>{"Know More-->"}</Text>
+                    <Text style={styles.bgDigit}>1</Text>
+                    <Image source = {reqire('../assets/meteor_icon.png')} style={styles.iconImage}></Image>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.routeCard}>
+                    <Text style={styles.routeText}>Updates</Text>
+                    <Text style={styles.knowMore}>{"Know More-->"}</Text>
+                    <Text style={styles.bgDigit}>1</Text>
+                    <Image source = {reqire('../assets/rocket_icon.png')} style={styles.iconImage}></Image>
+                </TouchableOpacity>
+                </ImageBackground>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex:1
+    },
+    titleText:{
+        color:'black',
+        fontSize:40
+    },
+    droidSafeArea:{
+        marginTop: Platfrom.OS ==="android"? StatusBar.currentHeight: 0
+    },
+    titleBar:{
+        justifyContent:"center",
+        alignItems:"center",
+        flex:0.15
+    },
+    routeCard:{
+        marginLeft:50,
+        marginRight:50,
+        marginTop: 50,
+        borderRadius:30,
+        flex:0.25,
+        backgroundColor:'white'
+    },
+    routeText: {
+        fontSize: 35,
+        fontWeight: 'bold',
+        paddingLeft: 30,
+        color: 'black'
+    },
+    backgroundImage: {
+        resizeMode: 'cover',
+        flex: 1
+    },
+    iconImage:{
+        height:200,
+        width:200,
+        resizeMode:"contain",
+        position:"absolute",
+        top:-80,
+        right:20
+    },
+    knowMore:{
+        color:"red",
+        paddingLeft:30,
+        fontSize:15
+    },
+    bgDigit:{
+        position:absolute,
+        fontSize:15,
+        color:"rgb(183, 183,183, 0.5)"
+    }
+})
